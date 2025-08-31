65fb71f18972465c06b5be042766128e324f0a82
sharifmasyhur <sharifalhadyyad@gmail.com>
2025-08-31 21:19:25 +0700

    done

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// index.js - React application entry point
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+// Create root element and render the App component
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
